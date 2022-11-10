29172f7397267e6c1fc1e0cdee44f0593dbd9259
Alyona Blintsova <asblintsova@mail.ru>
2022-11-10 21:11:46 +0700

    refactor: move AppInitializationWidget to separate file

--- lib/core/main/main.dart
+++ lib/core/main/main.dart
@@ -1,44 +1,6 @@
 import 'package:flutter/material.dart';
-
-import '../navigation/router.dart';
+import 'app_initialization_widget.dart';
 
 void main() {
   runApp(const AppInitializationWidget());
-}
-
-class AppInitializationWidget extends StatefulWidget {
-  const AppInitializationWidget({Key? key}) : super(key: key);
-
-  @override
-  State<AppInitializationWidget> createState() =>
-      _AppInitializationWidgetState();
-}
-
-class _AppInitializationWidgetState extends State<AppInitializationWidget> {
-  GoRouterWrapper? _routerWrapper;
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    _routerWrapper ??= GoRouterWrapper();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerDelegate: _routerWrapper!.goRouter.routerDelegate,
-      routeInformationParser: _routerWrapper!.goRouter.routeInformationParser,
-      routeInformationProvider:
-          _routerWrapper!.goRouter.routeInformationProvider,
-      title: 'Anime app',
-      //theme: AppThemes.lightTheme,
-      //darkTheme: AppThemes.darkTheme,
-    );
-  }
 }
