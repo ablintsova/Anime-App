b9dfb2683f3b32f4c6144557bbc72a66972c50e1
Alyona Blintsova <asblintsova@mail.ru>
2022-11-12 13:51:17 +0700

    feat(navigation)!: preserve tab state

--- lib/core/navigation/router.dart
+++ lib/core/navigation/router.dart
@@ -3,8 +3,8 @@ import 'package:anime_app/ui/favorites_page/favorites_page.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-import '../main/main_scaffold.dart';
 import 'app_routes.dart';
+import 'custom_shell_route.dart';
 
 class GoRouterWrapper {
   late final GoRouter _routerInstance;
@@ -19,22 +19,22 @@ class GoRouterWrapper {
       initialLocation: AppRoutes.explore,
       navigatorKey: _rootNavigatorKey,
       routes: [
-        ShellRoute(
-            navigatorKey: _shellNavigatorKey,
-            builder: (context, state, child) => MainScaffold(child: child),
-            routes: [
-              GoRoute(
-                path: AppRoutes.explore,
-                pageBuilder: (context, state) =>
-                    NoTransitionPage(key: state.pageKey, child: ExplorePage()),
-                routes: const [],
-              ),
-              GoRoute(
-                path: AppRoutes.favorites,
-                pageBuilder: (context, state) =>
-                    NoTransitionPage(key: state.pageKey, child: FavoritesPage()),
-              ),
-            ]),
+        BottomTabBarShellRoute(
+          navigatorKey: _shellNavigatorKey,
+          routes: [
+            GoRoute(
+              path: AppRoutes.explore,
+              pageBuilder: (context, state) => NoTransitionPage(
+                  key: state.pageKey, child: const ExplorePage()),
+              routes: const [],
+            ),
+            GoRoute(
+              path: AppRoutes.favorites,
+              pageBuilder: (context, state) =>
+                  NoTransitionPage(key: state.pageKey, child: FavoritesPage()),
+            ),
+          ],
+        ),
       ],
     );
   }
